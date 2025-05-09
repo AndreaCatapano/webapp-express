7d91044c049f9752efa1d453c006cb9a9709dc9d
AndreaCatapano <a.catapano96@gmail.com>
2025-05-09 15:41:08 +0200

    feat: added enviroment variable

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
+require('dotenv').config();
 const moviesRouter = require('./router/movie.js');
 
 
 const app = express();
-const port = 3000;
+const port = process.env.DB_PORT
 
 
 app.use('/movies', moviesRouter);
