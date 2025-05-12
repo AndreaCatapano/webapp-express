ec73dd109da5091bf88276568070fa4db36889a4
AndreaCatapano <a.catapano96@gmail.com>
2025-05-12 20:58:41 +0200

    fix: added url for cors

--- app.js
+++ app.js
@@ -12,7 +12,10 @@ const port = process.env.PORT || 3000
 app.use(express.static('public'))
 app.use(express.json())
 
-app.use(cors());
+app.use(cors({
+    origin: process.env.SERVER_URL
+}));
+
 
 app.use('/movies', moviesRouter);
 
